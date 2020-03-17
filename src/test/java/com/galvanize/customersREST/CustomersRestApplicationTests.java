561f678333562891d019204d781e41a7faef64b0
andrewtkemp@gmail.com <andrewtkemp@gmail.com>
2020-03-16 23:01:43 -0500

    initial

---
+++ src/test/java/com/galvanize/customersREST/CustomersRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.customersREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
